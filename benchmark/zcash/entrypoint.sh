533d2c808b4a6dd0048f26dac1dc436942d7e35a
Eugene Manaev <emanaev@gmail.com>
2018-02-20 14:44:50 +0300

    ZCASH benchmark

---
+++ benchmark/zcash/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./miner --server zec-eu1.nanopool.org --user $ZEC_ADDR.$WORKER/$EMAIL --pass z --port 6666 --api 127.0.0.1:42000 >/dev/null &
+sleep 60
+curl http://127.0.0.1:42000/getstat 2>/dev/null | jq '{devices:[.result[] | {pci_id: .busid, name: .name, value: .speed_sps} ]} | .benchmark="zcash" | .type="gpu"'
