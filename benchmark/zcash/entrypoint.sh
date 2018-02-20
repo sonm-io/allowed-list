9ceb136fb8248a7a1c15e340b8abb2757c6ded54
Eugene Manaev <emanaev@gmail.com>
2018-02-20 17:56:59 +0300

    Added cuda_id to benchmark

--- benchmark/zcash/entrypoint.sh
+++ benchmark/zcash/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ./miner --server zec-eu1.nanopool.org --user $ZEC_ADDR.$WORKER/$EMAIL --pass z --port 6666 --api 127.0.0.1:42000 >/dev/null &
 sleep 60
-curl http://127.0.0.1:42000/getstat 2>/dev/null | jq '{devices:[.result[] | {pci_id: .busid, name: .name, value: .speed_sps} ]} | .benchmark="zcash" | .type="gpu"'
+curl http://127.0.0.1:42000/getstat 2>/dev/null
+curl http://127.0.0.1:42000/getstat 2>/dev/null | jq '{devices:[.result[] | {cuda_id: .cudaid, pci_id: .busid, name: .name, value: .speed_sps} ]} | .benchmark="zcash" | .type="gpu"'
