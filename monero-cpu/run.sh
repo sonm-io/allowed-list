010b15555ce0ba3a826cf14b672fa3ef8f78fd1d
ALex Nikonov <alex@sshaman.ru>
2018-10-03 18:58:30 +0300

    aux: update xmrig to v2.8.4, add TLS flag

--- monero-cpu/run.sh
+++ monero-cpu/run.sh
@@ -3,13 +3,18 @@
 # USER-defined params
 SONM_POOL="${XMR_POOL:-xmr-eu1.nanopool.org:14444}"
 SONM_WORKER="${WORKER:-sonm_worker}"
+SONM_TLS_FLAG=""
 
 if [[ -z "$XMR_ADDR" ]]; then
     echo "Please set env-variable 'XMR_ADDR' for mined funds."
     exit 1
 fi
 
-/miner/xmrig -a cryptonight \
+if [[ ! -z "$USE_TLS" ]]; then
+    SONM_TLS_FLAG="--tls"
+fi
+
+/miner/xmrig -a cryptonight ${SONM_TLS_FLAG} \
     --url ${SONM_POOL} \
     --user ${XMR_ADDR} \
     --rig-id=${WORKER} \
