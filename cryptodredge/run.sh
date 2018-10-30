589a74bfb73d917e3c1228d946257c2f68d0a90f
avsigaev <avsigaev@gmail.com>
2018-10-30 11:01:37 +0300

    fix whitelist

---
+++ cryptodredge/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# monkey-patching for NVIDIA GPU ᕕ( ಠ_ಠ )ᕗ
+if [[ -d "/usr/local/nvidia/" ]]; then
+    echo "NVIDIA libs is mounted via SONM GPU plugin, applying patch..."
+    mkdir -p /lib64/nvidia/
+    ln -s /usr/local/nvidia/lib64/libnvidia-ml.so.1 /lib64/nvidia/libnvidia-ml.so
+    export LD_LIBRARY_PATH=/usr/local/nvidia/lib64/:/lib64/nvidia/
+fi
+
+# USER-defined params
+if [[ -z "$PARAMS" ]]; then
+    echo "Please set env-variable 'PARAMS' for mined funds."
+    exit 1
+fi
+
+/CryptoDredge ${PARAMS}
